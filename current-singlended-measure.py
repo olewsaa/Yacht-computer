22075dc445b8051238ed0f0064733d5eea90641c
olewsaa <olewsaa@online.no>
2017-06-11 18:26:53 +0200

    Add files via upload

---
+++ current-singlended-measure.py
@@ -0,0 +1,29 @@
+# Simple example of reading the MCP3008 analog input channels and printing
+# them all out.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+
+# Software SPI configuration:
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 25
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+
+while True:
+    value = mcp.read_adc(1)
+    i=(value-510)*0.09
+    print 'Value ',value,' current ',i,' A'
+    time.sleep(1)
