d19e655877c57fe163ed0e5ec1bbdd213717823e
Ole W. Saastad <olewsaa@online.no>
2017-08-12 18:11:12 +0200

    Add files via upload

--- PCF8574/tank-level.py
+++ PCF8574/tank-level.py
@@ -1,14 +1,16 @@
-# Distributed with a free-will license.
-# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
-# PCF8574
-# This code is designed to work with the PCF8574_LBAR_I2CL I2C Mini Module available from ControlEverything.com.
-# https://www.controleverything.com/products
+#!/usr/bin/env python
 
-import smbus
-import time
+
+# Aug 2017  OWS
+# Adapted to work with the PCF8574A based module.  
+
+import smbus, time, socket
 
 # Get I2C bus
 bus = smbus.SMBus(1)
+
+# Initiate socket 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # PCF8574  address, 0x20(32)
 # PCF8574A address, 0x38(56)
@@ -22,17 +24,14 @@ time.sleep(0.5)
 # Read data back, 1 byte
 data = bus.read_byte(0x38)
 
-
-# Convert the data
-# data = (data & 0xFF)
-
 num_sensors = 3
 interval = 1.0/float(num_sensors)
 
+SignalK=""
 while True:
     data = bus.read_byte(0x38)
     data = (data & 0xFF)
-    bus.write_byte(0x38,0xfe)
+# Pin 2,3 and 4 are used for sensors, 1/3, 2/3 and full.
     for i in range(2, 4) :
         level=0
         if data & (2 ** 2) == 0 :
@@ -41,8 +40,10 @@ while True:
             level=level+interval
         if data & (2 ** 4) == 0:
             level=level+interval    
-    print 'Tank level : ',format(level,'5.3f')
-    print 
     bus.write_byte(0x38,0xff)
+
+    SignalK='{"updates": [{"$source": "I2C.PCF8574A","values":[ {"path": "tanks.freshWater.*.currentLevel","value":'+format(level,'5.3f')+'}]}]}'
+    sock.sendto(SignalK, ('127.0.0.1', 55557))
     time.sleep(1)
-    
+        
+
