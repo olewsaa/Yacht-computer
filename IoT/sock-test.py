026aadc96386994c27af66ed48babaa7e20704b8
Ole W. Saastad <olewsaa@online.no>
2017-10-15 13:42:30 +0200

    Add files via upload

---
+++ IoT/sock-test.py
@@ -0,0 +1,17 @@
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# AF_INET Internet IP
+# SOCK_STREAM TCP
+
+
+server_address = ('192.168.1.111', 10002)
+sock.bind(server_address)
+sock.listen(1)
+while True:
+    connection, client_address = sock.accept()
+#    print connection
+    data = connection.recv(128)
+    print data
+
+
+connection.close()
