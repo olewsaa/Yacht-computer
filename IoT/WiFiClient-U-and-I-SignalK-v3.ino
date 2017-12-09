1861e81f9c19d7011c53416ef7b2e0a8d22e42fe
Ole W. Saastad <olewsaa@online.no>
2017-12-09 08:24:59 +0100

    Add files via upload

---
+++ IoT/WiFiClient-U-and-I-SignalK-v3.ino
@@ -0,0 +1,178 @@
+/*
+ *  This sketch uses a ESP 32 and ADS1115 to measure voltage and current and 
+ *  sends messages to a SignalK server
+ *  
+ *  Select board ESP32 , Node32s module
+ *
+ *  Analog voltage is single ended and read by the ESP analog inputs. 
+ *  
+ *  The current sensors emits Vdd/2 for zero current and voler that Vdd/2 for flow
+ *  in one direction and voltage higher than Vdd/2 for flow in the oposite direction,
+ *  Hence differential measurement is sensible as it yield negative values below Vdd/2 
+ *  and positive values above Vdd/2.
+ *
+ *  SignalK uses UDP. The syntax is differ between ESP8622 and ESP32.
+ *  
+ *  
+ */
+
+
+// Analog Input on the ESP32, this board has many analog inputs.
+#define ANALOG_PIN_0 36  // Pin GPIO 36
+int analog_value = 0;
+
+ 
+// Including I2C and ADC board ADS1115 
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+// Including WiFi stuff
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+/* WiFi network name and password */
+const char * ssid = "TeamRocketHQ";
+const char * pwd = "blackpearl";
+
+// IP address to send UDP data to.
+// it can be ip address of the server or 
+// Settings for SignalK port and SignalK server.
+const char * udpAddress = "192.168.1.160";
+const int udpPort = 55557;
+
+
+// Instanciate two of a maximum of four (48, 49, 4A & 4B) ads objects.
+Adafruit_ADS1115 ads1(0x48); 
+Adafruit_ADS1115 ads2(0x49); 
+
+// Instanciate WiFi object
+WiFiUDP Udp;
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
+  Serial.begin(115200);
+
+  //Connect to the WiFi network
+    WiFi.begin(ssid, pwd);
+    Serial.println("");
+
+  // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+    Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  //This initializes udp and transfer buffer
+    Udp.begin(udpPort);    
+    delay(10);
+
+  // Set up the first ADS 1115 gain
+    ads1.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 0.0625mV
+    ads1.begin();  
+    Serial.println("ADS 1115 no 1 set up");
+    ads2.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 0.0625mV
+    ads2.begin(); 
+    Serial.println("ADS 1115 no 2 set up");
+ 
+} /* End setup */
+
+
+
+void Send_to_SignalK(String path, double value){
+    String cmd;
+    char valuestring[6];
+    int len;
+
+  // Prepage the SignalK string to send.
+    cmd = "{\"updates\": [{\"$source\": \"ESP32\",\"values\":[ {\"path\":\"";
+    cmd += path; cmd += "\","; cmd += "\"value\":";
+    dtostrf(value,3,2,valuestring); // Convert double to a string
+    cmd += valuestring;
+    cmd += "}]}]}\0";
+    len=cmd.length(); // Get the lenght of message, needed by the udp write.
+    Serial.println(cmd); 
+    Serial.println(len);
+    
+    int j;
+  //data will be sent to server
+    uint8_t buffer[120]; // UDP write will only write sequence of bytes. 
+    for(j=0;j<len;j++){buffer[j]=cmd[j];} // Convert from char t bytes.
+    //send cmd to server
+    Udp.beginPacket(udpAddress, udpPort);
+    Udp.write(buffer, len); 
+    Udp.endPacket();
+    //Serial.println(cmd); 
+    delay(10); 
+   
+} /* End Send_to_SignalK */
+
+
+
+void Measure_and_Send_U(int inputno) {
+    double U; 
+    int val,offset;
+
+    /*
+     * Voltage is measured using the single ADC input on the ESP32 chip.
+     * However, this routine is generic and it can easily change to ADS1115. 
+     */
+    offset=-1;    // Differ from board to board, make your own calibration.
+    val=analogRead(ANALOG_PIN_0);  // Read the analog input on the ESP32.
+    val+=offset;
+    U=((double)val/(double)4095.0)*18.0;  // Calibrated using a multimeter, check this value carefully. 
+
+    Send_to_SignalK("electrical.service.voltage",U);
+}
+
+     
+void Measure_and_Send_I(int inputno) {
+    double I;
+    int val, offset;
+
+    if (inputno==1) {      
+      // Read ADC and calculate the equivalt current in Amps.
+      offset=-10;    // Differ from sensor to sensor, calibration is needed.
+      val=ads1.readADC_Differential_0_1();   // There are two possibilities in differential mode, 01 and 23.
+      Serial.println(val);
+      val+=offset;
+      I = (double)val/(double)32766*12.52; // Calibrated with a multimeter.
+      Send_to_SignalK("electrical.service.HKS2010",I);
+    }
+    if (inputno==2) {
+      offset=-130;
+      val=ads1.readADC_Differential_2_3();   // There are two possibilities in differential mode, 01 and 23.
+      Serial.println(val);
+      val+=offset;
+      I = (double)val/(double)32766*110.0; // Calibrated with a multimeter.
+      Send_to_SignalK("electrical.service.ACS712",I);
+    }
+    if (inputno==3) {
+      offset=0;
+      val=ads2.readADC_Differential_0_1();   // There are two possibilities in differential mode 01 and 23.
+      Serial.println(val);
+      val+=offset;
+      I = (double)val/(double)32766*33.0; // Calibrated with a multimeter.
+      Send_to_SignalK("electrical.service.WCS1800",I);
+    }
+}
+
+
+// The endless loop start here :
+
+void loop() {   
+    digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on while we transfer the data.
+    Measure_and_Send_U(1);  // Get the voltage and send it to SignalK.
+    Measure_and_Send_I(1);  // Get the current and send it to SignalK.
+    Measure_and_Send_I(2);  // Get the current and send it to SignalK.
+    //Measure_and_Send_I(3);  // Get the current and send it to SignalK.
+    digitalWrite(LED_BUILTIN, LOW);  // Turn the LED off by making the voltage LOW.
+    
+    Serial.println("wait 2 sec...");   // This high refresh rate put a load in the SignalK server.
+    delay(2000);
+}  /* End infinite loop */
+
