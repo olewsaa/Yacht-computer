0524742e11b40fd03c4aaea71d85ffd9a590ffed
Ole W. Saastad <olewsaa@online.no>
2019-02-12 12:36:27 +0100

    Gas detector Arduino code

---
+++ Gas-detector/MQ-sensors.ino
@@ -0,0 +1,38 @@
+
+/*
+
+  For Arduino NANO
+  Processor ATmega328P (old bootloader)
+  9600 baud
+
+  
+ */
+
+int value;
+int Alarm=0;
+const int analog_ip0 = A0;   //Naming analog input pin
+const int analog_ip1 = A1;   //Naming analog input pin
+
+void setup() {
+  Serial.begin(115200);
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);              // wait n ms
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);              // wait n ms 
+  value = analogRead(analog_ip0);
+  //Serial.print("Detector value MQ2 :"); 
+  //Serial.print(value);
+  if (value>500) Alarm=1;
+  value = analogRead(analog_ip1);
+  if (value>500) Alarm=1;
+  //Serial.print("  Detector value MQ9 :"); 
+  //Serial.print(value);
+  //Serial.println();
+  if (Alarm>0) Serial.println("ALARM ALARM ALARM ALARM");
+}
+
