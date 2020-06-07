6acaeacd275d65c0802aa64130d2c61978ae3c11
Ole W. Saastad <olewsaa@online.no>
2020-06-07 20:38:21 +0200

    Added SeatalkDataTest

---
+++ IoToB/Seatalk/SeatalkDataTest.ino
@@ -0,0 +1,350 @@
+
+
+/*
+ * 
+ * Seatalk data test
+ * 
+ * Demo sketch for testing receiving of Seatalk data. Uses SoftwareSerial to read 9-bit words  
+ * from Seatalk devices.
+ * 
+ * Ole W. Saastad,
+ * June 2020.
+ * 
+ * 
+ */
+
+
+/*
+ * 
+ * About SoftwareSerial.
+ * On ESP8266:
+ * At 80MHz runs up 57600ps, and at 160MHz CPU frequency up to 115200bps with only negligible errors.
+ * Connect pin 12 to 14.
+ *
+ * 
+ * Test for SoftwareSerial (to get en extra serial port on the ESP8266. 
+ * This example is part if initial testing using a SoftwareSerial 9 bit implementation.
+ * https://github.com/ionini/espsoftwareserial Downloaded and installed as under : ~/Arduino/libraries
+ * EspSoftwareSerial -> EspSoftwareSerial-9bit/
+ * EspSoftwareSerial-9bit
+ * EspSoftwareSerial-orig
+ *
+ * An additional change needed to be applied in the file EspSoftwareSerial/src/SoftwareSerial.cpp :
+ *  //attachInterruptArg(digitalPinToInterrupt(m_rxPin), (void (*)())(rxBitISR), this, CHANGE);
+ *  attachInterruptArg(digitalPinToInterrupt(m_rxPin), reinterpret_cast<void (*)(void*)>(rxBitISR), this, CHANGE);
+ * 
+ * An error in the casting of void function and viod parameters. Using the same line from the initial
+ * 8 bit version worked. 
+ * 
+ * The calling sequence is different in the original 8 bit versions and the extened 9 bit version.
+ * The 9 bit version looks like this: 
+ * swSerial.begin(BAUD_RATE, D5, D6, SWSERIAL_9N1, false, 95, 11); 
+ * Pin paramaters BEFORE the world lenght, parity and stop bits.
+ * 
+ * This testing is initial work in order to read 9-bit data from the Raymarine Seatalk data.
+ * 
+ */
+
+#define DEBUG
+
+#include <SoftwareSerial.h>
+
+ 
+#define D5 (14)   // D5 is the D5 pin on the board, also named GPIO 14, hence (14) which reflects the GPIO14
+#define D6 (12)   // D6 is the D6 pin on the board, also named GPIO 12, hence (12) which reflects the GPIO12
+
+// Reminder: the buffer size optimizations here, in particular the isrBufSize that only accommodates
+// a single 8N1 word, are on the basis that any char written to the loopback SoftwareSerial adapter gets read
+// before another write is performed. Block writes with a size greater than 1 would usually fail. 
+SoftwareSerial SwSerial;
+
+
+
+  
+void setup() {
+ 
+/*
+ * Set up the Serial port and the Software Serial port to 9 bits and Serial for logging.
+ * 
+ */ 
+
+  pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
+#ifdef DEBUG    
+  Serial.begin(115200);         // Setup Serial port speed
+#endif 
+
+  // Set up SoftwareSerial.  
+  // Baud rade 4800 (Seatalk), Rx pin D5, Tx pin D6, 9 bits word, No parity, 1 stop bit. Rest is std.
+  SwSerial.begin(4800, D5, D6, SWSERIAL_9N1, false, 95, 11);
+
+// The command bit is 0x01zz where zz are nibbles or byte with data. The command bit is the 9t bit.
+
+#ifdef DEBUG
+  Serial.println("\nSoftware serial test started");
+#endif
+} // End setup
+
+
+/*
+ * Seatalk data related constants, included for reference.
+ */
+
+#define DBT      0x00,0x02,0x44,0x01,0x60    //  (DBT): 00 02 YZ XX XX
+#define AWA      0x10,0x01,0x01,0x20         //  (AWA): 10 01 XX YY
+#define AWS      0x11,0x01,0x20,0x01         //  (AWS): 11 01 XX 0Y
+#define STW      0x20,0x01,0x01,0x20         //  (STW): 20 01 XX XX
+#define TRMIL    0x21,0x02,0x01,0x20,0x05    //  Trip Mileage XXXXX/100 nautical miles : 21 02 XX XX 0X
+#define TMIL     0x22,0x01,0x01,0x20,0x00    //  Total Mileage XXXXX/100 nautical miles : 22 02 XX XX 00
+#define WTEMP    0x27,0x01,0x01,0x20         //  (WTEMP) Water temp xxxx-100/10 Celsius: 27 01 XX XX
+
+
+// Empty function, just a dummy to handle calls to Send_to_SignalK .
+
+void Send_to_SignalK(String key, double value){
+#ifdef DEBUG
+  Serial.print(key); Serial.println(value);
+#endif
+  return;
+}
+/*
+ * Functions to handle the different Seatalk messages based on the message hex code receivced.
+ * 
+ * This code is not tested on real Seatalk data yet.
+ * 
+ */
+
+
+// Apparent wind speed 
+/*     
+ *  Apparent Wind Speed (AWS): 11 01 XX 0Y
+ *  (XX & 0x7F) + YY/10 = speed
+ *  XX&0x80=0           = knots (default)
+ *  XX&0x80=0x80        = meters/second
+*/
+int aws(){
+  unsigned int d;
+  char c1, c2;
+  float awsp;
+  float corr;
+  
+  d=SwSerial.read();
+#ifdef DEBUG
+  Serial.write("d HEX "); Serial.println(d, HEX);
+  if (d == 0x0001) Serial.write("Wind speed : ");
+#endif
+  d=SwSerial.read();
+  corr = (d & 0x80)?1.0:1.852;
+  c1 = lowByte((d & 0x7f));
+#ifdef DEBUG
+  Serial.write("d  HEX "); Serial.println(d, HEX);
+  Serial.write("c1 HEX ");Serial.println(c1, HEX);
+  Serial.write("corr "); Serial.println(corr);
+#endif
+  d=SwSerial.read();
+  c2=lowByte(d);
+#ifdef DEBUG
+  Serial.write("c2 HEX "); Serial.println(c2, HEX);
+#endif   
+  awsp=((float)c1+((float)c2)/10.0)/corr;
+#ifdef DEBUG
+  Serial.println(corr);
+  Serial.write("AWS : ");
+  Serial.println(awsp);
+#endif
+  Send_to_SignalK("environment.wind.speedApparent",awsp);
+}
+
+
+/*    
+ *  Apparent Wind Angle (AWA): 10 01 XX YY
+ *  
+ */
+int awa() {
+  unsigned int d;
+  char c1, c2; 
+  int awang;
+  
+  d=SwSerial.read();
+#ifdef DEBUG
+  Serial.println(d, HEX);
+  if (d == 0x0001) Serial.write("Wind angle : ");
+#endif
+  d=SwSerial.read();
+  c1=lowByte(d);
+#ifdef DEBUG  
+  Serial.println(c1, HEX);  
+#endif
+  d=SwSerial.read();
+  c2=lowByte(d);
+#ifdef DEBUG  
+  Serial.println(c2, HEX);
+#endif 
+  awang=makeWord(c1,c2);
+#ifdef DEBUG
+  Serial.println(awang);
+#endif
+  Send_to_SignalK("environment.wind.angleApparent",(awang/180.0)*3.1415);
+}
+
+
+/*    
+ *    Depth below Transducer (DBT): 00 02 YZ XX XX   
+ *    Y = 0 for feet (default), 4 for meters
+ *    Z = 1 for shallow depth alarm, 4 for defective transducter
+ *    XXXX = depth*10
+ */ 
+int dbt() {
+  unsigned int d;
+  char c1, c2; 
+  float dbt;
+  
+  int meters;
+  d=SwSerial.read();
+#ifdef DEBUG
+  Serial.println(d, HEX);
+  if (d == 0x0002) Serial.write("Depth : ");
+#endif
+  d=SwSerial.read();
+#ifdef DEBUG
+  Serial.println((d & 0x00F0), HEX);
+#endif
+  meters=((d & 0x00F0) == 0x40);
+#ifdef DEBUG
+  Serial.println(meters, HEX);
+#endif  
+  //Read the data XX XX 
+  d=SwSerial.read(); 
+#ifdef DEBUG 
+  Serial.println(d);
+#endif  
+  c1=lowByte(d);
+#ifdef DEBUG
+  Serial.println(c1, HEX);
+#endif
+  d=SwSerial.read();
+#ifdef DEBUG
+  Serial.println(d, HEX);
+#endif
+  c2=lowByte(d);
+#ifdef DEBUG
+  Serial.println(c2, HEX);
+#endif
+  d=makeWord(c1,c2);
+#ifdef DEBUG
+  Serial.println(d);
+#endif      
+  if (meters) dbt = (int)d/10.0; else dbt=(int)d/10.0*3.28;
+#ifdef DEBUG
+  Serial.println(dbt);
+#endif
+  Send_to_SignalK("environment.depth.belowTransducer",dbt);
+}
+
+
+/*
+ * Water temperature (WTEMP)
+ * WTEMP = (x - 100)/10
+ */
+int wtemp() {
+  unsigned int d;
+  char c1, c2;
+  float wtemp;
+  
+  d=SwSerial.read(); // Read the 01.
+#ifdef DEBUG
+  Serial.write("Water Temperature : ");
+#endif
+  // Read the data XX XX
+  d=SwSerial.read();
+  c1=lowByte(d);
+  d=SwSerial.read();
+  c2=lowByte(d);
+  d=makeWord(c1,c2); wtemp=(((float)d-100.0)/10.0);
+#ifdef DEBUG
+  Serial.println(wtemp);
+#endif
+  Send_to_SignalK("environment.water.temperature",(wtemp+273.15));
+}
+
+
+/*
+ * Speed through Water (STW): 20 01 XX XX
+ * XXXX = speed*10
+ * 
+ */
+int stw() { 
+  unsigned int d;
+  char c1, c2;
+  float stw;
+  
+  d=SwSerial.read(); // Read the 01.
+#ifdef DEBUG
+  Serial.println(d, HEX);
+  Serial.write("Speed through water : ");
+#endif
+  // Read the data XX XX
+  d=SwSerial.read();
+  c1=lowByte(d);
+  d=SwSerial.read();
+  c2=lowByte(d);
+  d=makeWord(c1,c2); stw=((float)d/10.0)/1.852; // SignalK is in m/s.
+#ifdef DEBUG
+  Serial.println(stw);
+#endif
+  Send_to_SignalK("navigation.speedThroughWater",stw);
+}
+
+
+
+
+/*
+ * Main loop start here, this is the endless loop.
+ */
+
+void loop() {
+  unsigned int d;
+  char c1, c2;
+
+  digitalWrite(LED_BUILTIN, LOW);  // Turn the LED on while we transfer the data.
+  
+  while (SwSerial.available() > 0) {
+    d=SwSerial.read();
+#ifdef DEBUG
+    Serial.println(d, HEX);
+    Serial.println((d & 0x0100), HEX);
+#endif 
+    if ((d & 0x0100) == 0) {  // Check if the command bit is set, if not just enter one more cycle.
+#ifdef DEBUG 
+      Serial.println("Error no command bit set");
+#endif 
+      continue; 
+    }  // if comand bit is set, this is the 9th bit. 
+     
+    d=(d & 0x00FF); // Strip off the comand bit. Only the least significant 8 bits in 
+                    // variable d (16 bits uint) are left.
+#ifdef DEBUG
+    Serial.println(d, HEX);
+#endif
+    // Apparent wind speed 
+    if (d == 0x0011) aws();
+    
+    // Apparent wind angle
+    if (d == 0x0010) awa();
+
+    // Depth
+    if (d == 0x0000) dbt();
+
+    // Speed through water
+    if (d == 0x0020) stw();
+
+    // Water temperature
+    if (d == 0x0027) wtemp();
+
+    yield(); // Let other tasks like wifi/tcp etc do their work.
+             //  The ESP8266 is single core, we do not have the luxery of multicore as the ESP32.
+  } // End while loop
+  
+  digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH.
+  delay(20000);
+  
+}  // End loop 
